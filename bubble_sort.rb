a9921bf7764ac37472bd4e607c9f917462c918c5
Danny Radden <dradden@gmail.com>
2017-01-26 18:26:09 -0700

    bubble_sort updated with alternate method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,9 +1,6 @@
-require 'pry'
-
 class BubbleSort
   def sort(collection)
-    length = collection.length
-    stage = length
+    stage = collection.length
     until stage == 1
       i = 0
       until i == (stage - 1)
@@ -24,4 +21,25 @@ sorter = BubbleSort.new
 
 puts sorter.sort(["d", "b", "a", "c", "f", "e"])
 
-collection[i, i + 1] = collection[i + 1, i]
+# Alternate Method
+#
+# class BubbleSort
+#   def sort(collection)
+#     stage = collection.length
+#     until stage == 1
+#       collection.each_with_index.each do |x, id|
+#         if id < stage - 1
+#           if x > collection[id + 1]
+#             collection[id, 2] = collection[id, 2].reverse
+#           end
+#         end
+#       end
+#       stage -= 1
+#     end
+#     collection
+#   end
+# end
+#
+# sorter = BubbleSort.new
+#
+# puts sorter.sort(["d", "b", "g", "c", "f", "e", "a"])
